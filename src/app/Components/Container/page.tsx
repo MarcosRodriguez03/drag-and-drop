f93a54a1ca4996ff09be5c033641855b7539a573
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-06-25 00:46:20 -0700

    d

--- src/app/Components/Container/page.tsx
+++ src/app/Components/Container/page.tsx
@@ -1,26 +1,29 @@
 'use client'
-import React, { useState } from 'react';
-import { UniqueIdentifier } from '@dnd-kit/core';
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+import { UniqueIdentifier } from '@dnd-kit/core'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import clsx from 'clsx';
+
+
+
+import React, { useState } from 'react'
 
 type ContainerProps = {
     id: UniqueIdentifier;
     children: React.ReactNode;
     title?: string;
-    description?: string;
-    closeMe: boolean;
+    closeMe: boolean
     onAddItem?: () => void;
-};
+}
 
-const Container: React.FC<ContainerProps> = ({
+
+
+const Container = ({
     id,
     children,
     title,
-    description,
-    closeMe,
     onAddItem,
+    closeMe
 }: ContainerProps) => {
     const [hide, setHide] = useState<string>('');
 
@@ -34,21 +37,20 @@ const Container: React.FC<ContainerProps> = ({
     } = useSortable({
         id: id,
         data: {
-            type: 'container',
+            type: 'container'
         },
     });
 
-    const handleCheck = () => {
-        console.log(id);
-        console.log(title);
-        console.log(children);
-        console.log(description);
-        console.log(closeMe);
-    };
+    const hanldeCheck = () => {
+        console.log(id)
+        console.log(title)
+        console.log(children)
+        console.log(closeMe)
+    }
 
     return (
         <div
-            onClick={() => alert('hi')}
+            onClick={() => alert("hi")}
             {...attributes}
             {...listeners}
             ref={setNodeRef}
@@ -58,36 +60,32 @@ const Container: React.FC<ContainerProps> = ({
             }}
             className={clsx(
                 'w-full h-full p-4 bg-gray-50 rounded-xl flex flex-col gap-y-4',
-                isDragging && 'opacity-50',
-                hide
+                isDragging && 'opacity-50', hide
             )}
         >
             <div className="flex items-center justify-between">
                 <div className="flex flex-col gap-y-1">
                     <h1 className="text-gray-800 text-xl">{title}</h1>
+
                 </div>
-                <button className="border p-2 text-xs rounded-xl shadow-lg hover:shadow-xl">
+                <button
+                    className="border p-2 text-xs rounded-xl shadow-lg hover:shadow-xl"
+                >
                     Drag Handle
                 </button>
             </div>
 
-            <button className="text-black" onClick={onAddItem}>
+
+            <button className='text-black' onClick={onAddItem}>
                 Add Item
             </button>
             <input type="checkbox" className="checkbox" />
 
-            <button
-                className="text-black"
-                onClick={(e) => {
-                    closeMe = false; // You cannot directly assign to closeMe if it's a prop, use state or props setter
-                    handleCheck();
-                    setHide('hidden');
-                }}
-            >
+            <button className='text-black' onClick={(e) => { closeMe = false, hanldeCheck(), setHide("hidden") }}>
                 Remove
             </button>
         </div>
-    );
-};
+    )
+}
 
-export default Container;
+export default Container
